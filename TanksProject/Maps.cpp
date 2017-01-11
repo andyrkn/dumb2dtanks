88cc4885cfd50feb5d3327d9ad43104e8450d891
Mihaila Andrei <andyfbgm@gmail.com>
2017-01-11 16:45:11 +0200

    new map

--- TanksProject/Maps.cpp
+++ TanksProject/Maps.cpp
@@ -1,5 +1,4 @@
 #include "Maps.h"
-#include "SFML\Graphics.hpp"
 
 
 Maps::Maps()
@@ -41,15 +40,20 @@ void Maps::MapInitialize2()
 
 void Maps::MapInitialize3()
 {
-	noWalls = 7;
-	walls[0].setSize(sf::Vector2f(1400.0f, 20.0f)); walls[0].setPosition(-100.0f, -10.0f); walls[0].setFillColor(sf::Color::Green);
-	walls[1].setSize(sf::Vector2f(1400.0f, 20.0f)); walls[1].setPosition(-100.0f, 590.0f); walls[1].setFillColor(sf::Color::Green);
-	walls[2].setSize(sf::Vector2f(20.0f, 700.0f)); walls[2].setPosition(-10.0f, -50.0f); walls[2].setFillColor(sf::Color::Green);
-	walls[3].setSize(sf::Vector2f(20.0f, 700.0f)); walls[3].setPosition(1190.0f, -50.0f); walls[3].setFillColor(sf::Color::Green);
-	walls[4].setSize(sf::Vector2f(800.0f, 30.0f)); walls[4].setPosition(180.0f, 130.0f); walls[4].setFillColor(sf::Color::Green);
-	walls[5].setSize(sf::Vector2f(800.0f, 30.0f)); walls[5].setPosition(180.0f, 430.0f); walls[5].setFillColor(sf::Color::Green);
-	walls[6].setSize(sf::Vector2f(30.0f, 300.0f)); walls[6].setPosition(950.0f, 130.0f); walls[6].setFillColor(sf::Color::Green);
-	//walls[7].setSize(sf::Vector2f(30.0f, 300.0f)); walls[7].setPosition(350.0f, 130.0f); walls[7].setFillColor(sf::Color::Green);
+	noWalls = 9;
+	walls[0].setSize(sf::Vector2f(1400.0f, 20.0f)); walls[0].setPosition(-100.0f, -10.0f); walls[0].setFillColor(sf::Color::Black);
+	walls[1].setSize(sf::Vector2f(1400.0f, 20.0f)); walls[1].setPosition(-100.0f, 590.0f); walls[1].setFillColor(sf::Color::Black);
+	walls[2].setSize(sf::Vector2f(20.0f, 700.0f)); walls[2].setPosition(-10.0f, -50.0f); walls[2].setFillColor(sf::Color::Black);
+	walls[3].setSize(sf::Vector2f(20.0f, 700.0f)); walls[3].setPosition(1190.0f, -50.0f); walls[3].setFillColor(sf::Color::Black);
+
+	walls[4].setSize(sf::Vector2f(40.0f, 400.0f)); walls[4].setPosition(200.0f, 100.0f); walls[4].setFillColor(sf::Color::Magenta); walls[4].setSize(sf::Vector2f(40.0f, 400.0f)); walls[4].setPosition(200.0f, 100.0f); walls[4].setFillColor(sf::Color::Magenta);
+	walls[5].setSize(sf::Vector2f(40.0f, 400.0f)); walls[5].setPosition(700.0f, 100.0f); walls[5].setFillColor(sf::Color::Magenta);
+	walls[6].setSize(sf::Vector2f(40.0f, 400.0f)); walls[6].setPosition(1000.0f, 100.0f); walls[6].setFillColor(sf::Color::Magenta);
+
+	walls[7].setSize(sf::Vector2f(300.0f, 40.0f)); walls[7].setPosition(200.0f, 100.0f); walls[7].setFillColor(sf::Color::Magenta);
+	walls[8].setSize(sf::Vector2f(300.0f, 40.0f)); walls[8].setPosition(200.0f, 300.0f); walls[8].setFillColor(sf::Color::Magenta);
+
+	
 }
 
 void Maps::draw(sf::RenderWindow & window)
